51a5d96a0b1555f34fdacfe93d69096272bbd73b
Yotam Schreiber <yschreiber@optimove.com>
2016-07-03 16:36:07 +0300

    first commit web site

---
+++ Ex3/Server.js
@@ -0,0 +1,126 @@
+/**
+ * Ex3 - 302856026 - Yotam Schreiber.
+ */
+
+
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var session = require('express-session');
+app.use(bodyParser());
+app.use(session({ secret: 'yotam', cookie: { maxAge: 6000000 }}));
+app.use('/', express.static(__dirname + '/www'));
+
+// session var
+var sess;
+
+// dictionary with the leagal admins details
+var dic = {
+    'admin': 'admin',
+    '302856026': 'yotam'
+};
+
+app.get('/', function (req, res) {
+    res.sendFile( __dirname + "/www/profile.html" );
+});
+
+app.get('/isLogin', function (req, res) {
+    sess = req.session;
+    if(sess.username) {
+        res.json('true');
+    } else {
+        res.json('false');
+    }
+    res.end();
+});
+
+
+app.post('/login', function (req, res) {
+    var username = req.body.username;
+    var password = req.body.userPassword;
+
+    if((username === 'admin' && password === dic.admin) || (username === '302856026' && password === dic[username])) {
+        // creating an object to send back and tell if the login operation was success
+        var successLoginDetails = {
+            'isSuccess': 'true',
+            'username': username,
+            'password': password
+        };
+         sess = req.session;
+         sess.username = username;
+	     res.json(successLoginDetails);
+    } else {
+        // creating an object to send back and tell if the login operation was success
+        var notSuccessLoginDetails = {
+            'isSuccess': 'false',
+            'username': username,
+            'password': password
+        };
+        sess = req.session;
+        sess.username = username;
+        res.json(notSuccessLoginDetails);
+    }
+    
+    res.end();
+});
+
+// vars contains the last value that has been clculated
+var lastResultAdmin = 0;
+var lastResultYotam = 0;
+
+app.get('/calc/value/', function (req, res) {
+    sess = req.session;
+    if(sess.username) {
+        if(sess.username === 'admin') {
+            res.json(lastResultAdmin);
+        } else if(sess.username === '302856026') {
+            res.json(lastResultYotam);
+        }
+    }
+    res.end();
+});
+
+app.post('/calc/value/:val', function (req, res) {
+    sess = req.session;
+    if(sess.username) {
+        if(sess.username === 'admin') {
+            lastResultAdmin = req.params.val;
+        }  else if(sess.username === '302856026') {
+            lastResultYotam = req.params.val;
+        }
+    }
+    res.end();
+});
+
+var quotes = ['I believe that if life gives you lemons, you should make lemonade... And try to find somebody whose life has given them vodka, and have a party.',
+    'One advantage of talking to yourself is that you know at least somebody is listening.',
+    'Laugh and the world laughs with you, snore and you sleep alone.'];
+
+app.get('/quotes/random', function (req, res) {
+    sess = req.session;
+
+    if(sess.username) {
+        var randomNumber = Math.random();
+
+        if((randomNumber >= 0) && (randomNumber < 0.33)) {
+            res.send(quotes[0]);
+        } else if((randomNumber >= 0.33) && (randomNumber < 0.66)) {
+            res.send(quotes[1]);
+        } else {
+            res.send(quotes[2]);
+        }
+    } else {
+        res.send('You are not login!');
+    }
+
+    res.end();
+});
+
+
+var server = app.listen(8081, function () {
+
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log("Example app listening at http://%s:%s", host, port)
+});
---
+++ Ex3/www/ex2Logic.js
@@ -0,0 +1,197 @@
+/**
+ * Ex3 - 302856026 - Yotam Schreiber.
+ */
+
+var lb = document.getElementById("loginButton");
+var pb = document.getElementById("profileButton");
+var cb = document.getElementById("calculatorButton");
+var rb = document.getElementById("readmeButton");
+var plus = document.getElementById("+");
+var minus = document.getElementById("-");
+var mult = document.getElementById("*");
+var divide = document.getElementById("/");
+
+var vs = document.getElementById("virtual screen");
+var pf = document.getElementById("formProfile");
+var cf = document.getElementById("formCalculator");
+var rf = document.getElementById("formReadme");
+var fw = document.getElementById("formWrong");
+
+var numberOnly = function (e) {
+    var a = [];
+    var k = e.which;
+
+    for (i = 48; i < 58; i++) {
+        a.push(i);
+    }
+
+    a.push(45);
+
+    if (!(a.indexOf(k)>=0))
+        e.preventDefault();
+};
+
+// in order to check if the user is already loged-in
+$(document).ready(function(){
+    $.ajax({
+        url: '/isLogin',
+        type: 'GET',
+        success: function (data) {
+            if(data == 'true') {
+                vs.style.display = "none";
+                fw.style.display = "none";
+                rb.style.display = "block";
+                pb.style.display = "block";
+                cb.style.display = "block";
+            } else {
+                vs.style.display = "block";
+            }
+        }
+    });
+});
+
+// log-in form
+lb.addEventListener("click", function() {
+    var userInfo = {
+        'username' : $('input[id=Name]').val(),
+        'userPassword' : $('input[id=Password]').val()
+    };
+
+    $.ajax({
+        url: '/login',
+        type: 'POST',
+        data: userInfo,
+        dataType: 'json',
+		success: function (data) {
+            // if the login operation was success
+			if(data.isSuccess == 'true') {
+            
+                vs.style.display = "none";
+                fw.style.display = "none";
+                rb.style.display = "block";
+                pb.style.display = "block";
+                cb.style.display = "block";
+                
+			} else {
+                
+                fw.style.display = "block";
+                
+            }
+        }
+    });
+});
+
+pb.addEventListener("click", function () {
+
+    // getting a random qoute
+    $.ajax({
+        url: '/quotes/random',
+        type: 'GET',
+        success: function (data) {
+            $('#formRandomQuote').text(data);
+        }
+    });
+
+    pf.style.display = "block";
+    cf.style.display = "none";
+    rf.style.display = "none";
+});
+
+cb.addEventListener("click", function () {
+
+    // getting a the last calculated value
+    $.ajax({
+        url: '/calc/value/',
+        type: 'GET',
+        success: function (data) {
+            console.log(data);
+            var r = document.getElementById("resultNum");
+            r.value = data;
+        }
+    });
+
+    pf.style.display = "none";
+    cf.style.display = "block";
+    rf.style.display = "none";
+});
+
+rb.addEventListener("click", function () {
+    pf.style.display = "none";
+    cf.style.display = "none";
+    rf.style.display = "block";
+});
+
+plus.addEventListener("click", function () {
+    var x = document.getElementById("firstNum");
+    var y = document.getElementById("secondNum");
+    var r = document.getElementById("resultNum");
+    r.value = Number(x.value) + Number(y.value);
+    var val = r.value;
+
+    // send the calculated value
+    $.ajax({
+        url: '/calc/value/' + val,
+        type: 'POST',
+        success: function (data) {
+
+        }
+    });
+});
+
+minus.addEventListener("click", function () {
+    var x = document.getElementById("firstNum");
+    var y = document.getElementById("secondNum");
+    var r = document.getElementById("resultNum");
+    r.value = Number(x.value) - Number(y.value);
+    var val = r.value;
+
+    // send the calculated value
+    $.ajax({
+        url: '/calc/value/' + val,
+        type: 'POST',
+        success: function (data) {
+
+        }
+    });
+});
+
+mult.addEventListener("click", function () {
+    var x = document.getElementById("firstNum");
+    var y = document.getElementById("secondNum");
+    var r = document.getElementById("resultNum");
+    r.value = Number(x.value) * Number(y.value);
+    var val = r.value;
+
+    // send the calculated value
+    $.ajax({
+        url: '/calc/value/' + val,
+        type: 'POST',
+        success: function (data) {
+
+        }
+    });
+});
+
+divide.addEventListener("click", function () {
+    var x = document.getElementById("firstNum");
+    var y = document.getElementById("secondNum");
+    var r = document.getElementById("resultNum");
+    if(y.value == 0) {
+        r.value = "Error";
+    } else {
+        r.value = Number(x.value) / Number(y.value);
+    }
+    var val = r.value;
+
+    // send the calculated value
+    $.ajax({
+        url: '/calc/value/' + val,
+        type: 'POST',
+        success: function (data) {
+
+        }
+    });
+});
+
+document.getElementById('firstNum').addEventListener('keypress', numberOnly, false);
+document.getElementById('secondNum').addEventListener('keypress', numberOnly, false);
